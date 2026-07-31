63c193f02898331437a71e1b4c7fe1e8de84085b
saumy <saumyadivyanjalee529@gmail.com>
2026-07-31 19:49:54 +0530

    updated pricing-service

--- parking-space-service/src/main/java/lk/ijse/ad/parkingspaceservice/service/PricingService.java
+++ parking-space-service/src/main/java/lk/ijse/ad/parkingspaceservice/service/PricingService.java
@@ -5,7 +5,10 @@ import java.time.LocalTime;
 
 @Service
 public class PricingService {
-    public double calculatePrice(double basePrice) {
+    public double calculatePrice(Double basePrice) {
+        if (basePrice == null) {
+            return 0.0; // හෝ default price එකක් දෙන්න
+        }
         LocalTime now = LocalTime.now();
         boolean isPeakHour =
                 (now.isAfter(LocalTime.of(7, 0)) && now.isBefore(LocalTime.of(9, 0))) ||
